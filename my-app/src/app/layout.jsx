a5823daebd5476fcfc2705775056656b3e32bf1e
l-tting <brianletting01@gmail.com>
2026-02-23 23:24:54 +0300

    favicon test

--- my-app/src/app/layout.jsx
+++ my-app/src/app/layout.jsx
@@ -2,18 +2,22 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "Your App Name",
+  description: "Your description",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* 1. Added flex, flex-col, and min-h-screen to the body */}
       <body className="flex flex-col min-h-screen bg-stone-100">
         <Navbar />
-        
-        {/* 2. Wrap children in a main tag with flex-grow */}
         <main className="flex-grow">
           {children}
         </main>
-        
         <Footer />
       </body>
     </html>

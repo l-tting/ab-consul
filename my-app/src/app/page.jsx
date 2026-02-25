0102ff84f7d956514377d625f4422339f16bd813
l-tting <brianletting01@gmail.com>
2026-02-25 10:12:06 +0300

    home page hero section CTA v2

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -9,23 +9,13 @@ const HomePage = () => {
 
     return (
         <div>
-            <section className="ml-4 ">
-                <p className="text-xl font-medium uppercase tracking-widest text-muted-foreground mb-2 mt-4">
-                    AB CONSUL
-                </p>
-
-                <h2 className="text-xl font-semibold tracking-tight mb-6">
-                    Consulting services built for modern teams
-                </h2>
-
-
-            </section>
+           
 
             {/* Hero comp */}
             <div>
                 <HeroHome1 />
             </div>
-            
+
             <div>
                 <HeroHome />
             </div>

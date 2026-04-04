7967331e02a03ac06cee3950ff4d752208a90c2f
l-tting <brianletting01@gmail.com>
2026-04-04 16:16:06 +0300

    test mail

--- my-app/src/app/api/send_mail/route.js
+++ my-app/src/app/api/send_mail/route.js
@@ -26,7 +26,7 @@ export async function POST(request) {
 
     const { data, error } = await resend.emails.send({
       from: "no-reply@ab-consul.com",
-      to: "brianletting01@gmail.com", // or use `to`
+      to: "admin@ab-consul.com", // or use `to`
       subject: subject,
       html: html,
     });

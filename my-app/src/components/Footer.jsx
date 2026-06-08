c9da2723190187d1d3c711c5d3c9a5c84e165249
l-tting <brianletting01@gmail.com>
2026-06-08 22:18:07 +0300

    removed x social link in footer

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -9,15 +9,15 @@ function Footer() {
   const pathname = usePathname();
 
   const socialIcons = [
-    {
-      name: "X",
-      href: "https://x.com/",
-      svg: (
-        <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
-          <path d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"></path>
-        </svg>
-      ),
-    },
+    // {
+    //   name: "X",
+    //   href: "https://x.com/",
+    //   svg: (
+    //     <svg className="h-5 w-5" viewBox="0 0 24 24" fill="currentColor">
+    //       <path d="M10.488 14.651L15.25 21h7l-7.858-10.478L20.93 3h-2.65l-5.117 5.886L8.75 3h-7l7.51 10.015L2.32 21h2.65zM16.25 19L5.75 5h2l10.5 14z"></path>
+    //     </svg>
+    //   ),
+    // },
     {
       name: "LinkedIn",
       href: "https://www.linkedin.com/company/ab-consul/",

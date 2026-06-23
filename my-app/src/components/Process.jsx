c2d7957bf70cd359e9b5692295edb1458ca2d3ae
l-tting <brianletting01@gmail.com>
2026-06-23 12:39:40 +0300

    testing badge margin from nav on md and lg

--- my-app/src/components/Process.jsx
+++ my-app/src/components/Process.jsx
@@ -35,7 +35,7 @@ const steps = [
 
 export default function Process() {
   return (
-    <section className="w-full py-14 md:py-12 mb-8">
+    <section className="w-full py-14 md:py-12 ">
       <div className="max-w-6xl mx-auto px-5 md:px-6">
 
         {/* Heading */}

70795cc3795cf06a55eca8b9e45b55faaeb1f75f
l-tting <brianletting01@gmail.com>
2026-06-23 14:02:19 +0300

    added logo....

--- my-app/src/components/AbLogo.jsx
+++ my-app/src/components/AbLogo.jsx
@@ -4,8 +4,8 @@ import React from "react";
 
 export default function ABLogo() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-stone-100">
-      <span className="text-8xl md:text-9xl font-black tracking-tighter text-slate-900">
+    <div className="flex items-center justify-center  bg-stone-100">
+      <span className="text-4xl md:text-4xl font-black tracking-tighter text-slate-900">
         A<span className="text-blue-600">B</span>
       </span>
     </div>

1f86f703d0bef14998bc7c5e5e050cb68fed5a03
l-tting <brianletting01@gmail.com>
2026-06-23 14:14:23 +0300

    added logo....

--- my-app/src/components/AbLogo.jsx
+++ my-app/src/components/AbLogo.jsx
@@ -4,9 +4,17 @@ import React from "react";
 
 export default function ABLogo() {
   return (
-    <div className="flex items-center justify-center  bg-stone-100 py-8">
-      <span className="text-4xl md:text-4xl font-black tracking-tighter text-slate-900">
-        A<span className="text-blue-600">B</span>
+    <div className="flex items-center justify-center bg-stone-100 py-8">
+      {/* 'group' on the parent triggers changes in children on hover */}
+      <span className="group text-4xl md:text-4xl font-black tracking-tighter text-slate-900 cursor-pointer">
+        {/* 'A' slides slightly up and left */}
+        <span className="inline-block transition-transform duration-300 ease-out group-hover:-translate-y-1 group-hover:-translate-x-0.5">
+          A
+        </span>
+        {/* 'B' slides slightly down and right */}
+        <span className="inline-block text-blue-600 transition-transform duration-300 ease-out group-hover:translate-y-1 group-hover:translate-x-0.5">
+          B
+        </span>
       </span>
     </div>
   );
